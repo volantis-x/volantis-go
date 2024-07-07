6e47ba5f0bf27cfe1682802142b2e0ed43015dd1
Tikas <Tikas@qq.com>
2024-07-07 18:01:22 +0800

    wip: 使用：检查、预加载、语义化 emoji 插件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  // prefetch 开启链接预加载
+  prefetch: true,
+  markdown: {
+    // 在 md 和 mdx 文件中语义化 emoji 图标
+    rehypePlugins: [rehypeAccessibleEmojis],
+  },
 });
