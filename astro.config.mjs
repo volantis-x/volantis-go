289f4496a9e1c53c333cf1eec88ef3368079284b
Tikas <Tikas@qq.com>
2024-07-14 11:42:19 +0800

    wip: 启用网站地图 Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   // prefetch 开启链接预加载
   prefetch: true,
   markdown: {
     // 在 md 和 mdx 文件中语义化 emoji 图标
-    rehypePlugins: [rehypeAccessibleEmojis],
-  },
+    rehypePlugins: [rehypeAccessibleEmojis]
+  }
 });
