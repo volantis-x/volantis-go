41cd230f5db492dc3d7ad996a8e2523b00fbd8b9
Tikas <Tikas@qq.com>
2024-08-18 21:40:25 +0800

    perf: 把时间也加进控制台的警告颜色，进一步友好提示

--- src/helpers/config/console.ts
+++ src/helpers/config/console.ts
@@ -1,3 +1,3 @@
 import { getTimeStamp } from "@/utils/utils";
 
-export const WARNING: string = `${getTimeStamp()} \u001b[33m ⚠ WARNING: \u001b[0m`;
+export const WARNING: string = `\u001b[33m${getTimeStamp()} ⚠ WARNING: \u001b[0m`;
