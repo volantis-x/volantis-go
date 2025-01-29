52494719173a383e2ab1f167111578fa3bf4cdbb
Tikas <Tikas@qq.com>
2025-01-29 15:39:00 +0800

    pom: 增加 MarkDOC 格式支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { SITE } from "./src/content/site.config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import markdoc from "@astrojs/markdoc";
+
 // 判断是否有设置网站地址
 // Determine whether a website address has been set
 const CUSTOM_SITE = SITE !== undefined && SITE !== "http://example.com" && SITE;
@@ -12,7 +14,7 @@ const site = CUSTOM_SITE || "http://localhost";
 // https://astro.build/config
 export default defineConfig({
   site,
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), markdoc()],
   // prefetch 开启链接预加载
   prefetch: true,
   markdown: {
